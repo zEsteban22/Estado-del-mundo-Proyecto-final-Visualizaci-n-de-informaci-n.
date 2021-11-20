49c7b827f7880cc16364efa06b56d8a8fc1470b6
Esteban Cruz <xllestebanllx@estudiantec.cr>
2021-11-20 12:31:09 -0600

    ejemplo del PC1

--- Datos/VisualizacionProyeccionPoblacional/prueba.py
+++ Datos/VisualizacionProyeccionPoblacional/prueba.py
@@ -1,42 +1,78 @@
-import plotly.graph_objects as go
-
+import plotly.express as px
 import pandas as pd
+import json
+from math import log
+import dash
+from dash import dcc
+from dash import html
+with open("data.json") as doc:
+    l = json.load(doc)['data']
+    for i in range(len(l)):
+        l[i].update(val=log(l[i]['Trade Value'], 2))
+    df = pd.DataFrame(l)
 
-df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/2014_us_cities.csv')
-df.head()
+colorDiscreteMap = {
+    "Animal Products": "rgba(170,100, 57,1)",
+    "Vegetable Products": "rgba( 45,134, 50,1)",
+    "Animal and Vegetable Bi-Products": "rgba(170,170, 57,1)",
+    "Foodstuffs": "rgba(155, 52, 78,1)",
+    "Mineral Products": "rgba( 35, 98,103,1)",
+    "Chemical Products": "rgba(108, 37,111,1)",
+    "Plastics and Rubbers": "rgba( 51, 53,119,1)",
+    "Animal Hides": "rgba(144,164, 55,1)",
+    "Wood Products": "rgba( 35,100,103,1)",
+    "Paper Goods": "rgba(  0,157,141,1)",
+    "Textiles": "rgba( 62, 52, 35,1)",
+    "Footwear and Headwear": "rgba( 38, 89,106,1)",
+    "Stone And Glass": "rgba( 88,145,163,1)",
+    "Precious Metals": "rgba(149, 75,  0,1)",
+    "Metals": "rgba(149, 11,  0,1)",
+    "Machines": "rgba( 31,  9,103,1)",
+    "Transportation": "rgba(123,159, 53,1)",
+    "Instruments": "rgba(170,169, 57,1)",
+    "Weapons": "rgba( 77, 45,115,1)",
+    "Miscellaneous": "rgba(103, 65, 64,1)",
+    "Arts and Antiques": "rgba(151, 50, 83,1)"
+}
+sunburstFig = px.sunburst(df, path=['Year', 'Section', 'HS2', 'HS4'],
+                          color='val', color_continuous_scale='rdbu',
+                          title="Gráfico radial",
+                          width=1400, height=800)
+sunburstFig.update_layout(
+    font_family="Verdana",
+    font_color="blue",
+    title_font_family="Times New Roman",
+    legend_title_font_color="black"
+)
+treemapFig = px.icicle(df, path=['Year', 'Section', 'HS2', 'HS4'],
+                       color='Section', values='Trade Value', title="Gráfico de icicle hacia la derecha.",
+                       color_discrete_map=colorDiscreteMap,
+                       width=1400, height=800)
+treemapFig.update_layout(
+    font_family="Verdana",
+    font_color="blue",
+    title_font_family="Times New Roman",
+    legend_title_font_color="black"
+)
 
-df['text'] = df['name'] + '<br>Population ' + (df['pop']/1e6).astype(str)+' million'
-limits = [(0,2),(3,10),(11,20),(21,50),(50,3000)]
-colors = ["royalblue","crimson","lightseagreen","orange","lightgrey"]
-cities = []
-scale = 5000
 
-fig = go.Figure()
+icicleFig = px.icicle(df, path=['Year', 'Section', 'HS2', 'HS4'],
+                      color='Section', values='Trade Value', color_discrete_map=colorDiscreteMap,
+                      title="Gráfico de icicle hacia abajo.",
+                      width=1400, height=800)
+icicleFig.update_traces(tiling=dict(orientation='v'))
+icicleFig.update_layout(
+    font_family="Verdana",
+    font_color="blue",
+    title_font_family="Times New Roman",
+    legend_title_font_color="black"
+)
+app = dash.Dash()
+app.layout = html.Div([
+    html.H1("Gráficos de las exportaciones ticas en el año 2019 generados por la liberaría plotly de python"),
+    dcc.Graph(figure=sunburstFig),
+    dcc.Graph(figure=treemapFig),
+    dcc.Graph(figure=icicleFig)
+])
 
-for i in range(len(limits)):
-    lim = limits[i]
-    df_sub = df[lim[0]:lim[1]]
-    fig.add_trace(go.Scattergeo(
-        locationmode = 'USA-states',
-        lon = df_sub['lon'],
-        lat = df_sub['lat'],
-        text = df_sub['text'],
-        marker = dict(
-            size = df_sub['pop']/scale,
-            color = colors[i],
-            line_color='rgb(40,40,40)',
-            line_width=0.5,
-            sizemode = 'area'
-        ),
-        name = '{0} - {1}'.format(lim[0],lim[1])))
-
-fig.update_layout(
-        title_text = '2014 US city populations<br>(Click legend to toggle traces)',
-        showlegend = True,
-        geo = dict(
-            scope = 'usa',
-            landcolor = 'rgb(217, 217, 217)',
-        )
-    )
-
-fig.show()
+app.run_server()
